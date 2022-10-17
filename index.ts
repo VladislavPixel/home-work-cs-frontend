9d8598fb68af6a848399253ecc260fec28ca4a11
pixel.inc <pixel.develop@yandex.ru>
2022-10-17 16:48:59 +0300

    initialization files

---
+++ index.ts
@@ -0,0 +1,15 @@
+import LinkedListWithIterator from "./lecture1/task1.1linked-list/modules/linked-list";
+import LinkedList from "./lecture1/task1linked-list/modules/linked-list";
+import Queue from "./lecture1/task2queue/module/queue";
+import DecQueue from "./lecture1/task3dec-queue/module/dec-queue";
+import Stack from "./lecture1/task4stack/module/stack";
+import Structure from "./lecture1/task5structure/module/structure";
+
+export {
+  LinkedListWithIterator,
+  LinkedList,
+  Queue,
+  DecQueue,
+  Stack,
+  Structure
+};
